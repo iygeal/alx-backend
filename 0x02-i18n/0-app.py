41f4edf5382cf952daab38c76baa2faf2358846b
Innocent Anozie <iygeal@gmail.com>
2024-09-10 20:18:46 +0100

    FIX: Added type annotation

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,8 +7,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    """ Returns the rendered index.html template"""
+def index() -> str:
+    """Returns the rendered index.html template"""
     return render_template('0-index.html')
 
 if __name__ == "__main__":
